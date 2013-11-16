a26ef7aa81c131489ff7a3e05e60f64326b3e557
Marcin Dudek <mdumaro@gmail.com>
2013-11-16 22:20:56 +0200

    parser_working

--- parser.cpp
+++ parser.cpp
@@ -6,62 +6,52 @@ using namespace std;
  
 int main()
 {
-int width,height;
-int i,j;
-int n;
-int **cords, **window ;
+unsigned int width,height;
+unsigned int i,j;
+unsigned int n;
+unsigned short int **cords, **window ;
 
 
 fstream tFile("input.txt",ios::in | ios::out | ios::ate);
 tFile.close();
  
 tFile.open("input.txt");
-while(!tFile.eof())
+if (tFile.good())
 {
 	tFile >> width >> height;
+
+    cords = new unsigned short int * [width]; 
+    for (i = 0; i<width; i++)
+        cords[i] = new unsigned short int [height];
 	
-	cords = (int**)malloc(width * sizeof(int*));
-    for (i=0; i<width; i++ ) 
-        cords[i] = (int*)malloc(height * sizeof(int)); 
-	
-	for(i = 0;i<width;i++)
+	for(i = 0;i<height;i++)
 	{
-		for(j = 0;i<height;j++)
+		for(j = 0;j<width;j++)
 		{
 			tFile >> cords[i][j];
+				
 		}
 		
+	
 	}
 	
 	tFile >> n;
 	
-	window = (int**)malloc(n * sizeof(int*));
-    for (i=0; i<width; i++ ) 
-        window[i] = (int*)malloc(height * sizeof(int));
+	window = new unsigned short int * [n]; 
+    for (i = 0; i<n; i++)
+        window[i] = new unsigned short int [4];
 	
 	for(i = 0;i<n;i++)
 	{
-		for(j = 0;i<height;j++)
+		for(j = 0;j<4;j++)
 		{
 			tFile >> window[i][j];
 		}
-		
 	}
 	
 }
-
-cout<<width<<height;
-for(i = 0;i<width;i++)
-	{
-		for(j = 0;i<height;j++)
-		{
-			cout << cords[i][j];
-		}
-		
-	}
-
 tFile.close();
- 
+
 system("PAUSE");
 return 0;
 }
