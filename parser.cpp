ec0fcdf7f312b8b419d63071485017295a4c7f2c
bhennes <baptiste.hennes@utbm.fr>
2013-11-16 22:57:21 +0200

    parser

--- parser.cpp
+++ parser.cpp
@@ -2,56 +2,61 @@
 #include <fstream>
 #include <stdlib.h>
 
+#include "parser.h"
 using namespace std;
- 
-int main()
+
+void readTable(USI ***pnCoord,USI ***pnWindow, int *pnWidth,int *pnHeight,int *pnNbWindow)
 {
-unsigned int width,height;
-unsigned int i,j;
-unsigned int n;
-unsigned short int **cords, **window ;
+    USI **cords,**window;
+    unsigned int width,height;
+    unsigned int i,j;
+    unsigned int n;
+    //unsigned short int **cords, **window ;
 
 
-fstream tFile("input.txt",ios::in | ios::out | ios::ate);
-tFile.close();
- 
-tFile.open("input.txt");
-if (tFile.good())
-{
-	tFile >> width >> height;
+    fstream tFile("input.txt",ios::in | ios::out | ios::ate);
+    tFile.close();
 
-    cords = new unsigned short int * [width]; 
-    for (i = 0; i<width; i++)
-        cords[i] = new unsigned short int [height];
-	
-	for(i = 0;i<height;i++)
-	{
-		for(j = 0;j<width;j++)
-		{
-			tFile >> cords[i][j];
-				
-		}
-		
-	
-	}
-	
-	tFile >> n;
-	
-	window = new unsigned short int * [n]; 
-    for (i = 0; i<n; i++)
-        window[i] = new unsigned short int [4];
-	
-	for(i = 0;i<n;i++)
-	{
-		for(j = 0;j<4;j++)
-		{
-			tFile >> window[i][j];
-		}
-	}
-	
-}
-tFile.close();
+    tFile.open("input.txt");
+    if (tFile.good())
+    {
+        tFile >> width >> height;
 
-system("PAUSE");
-return 0;
+        cords = new unsigned short int * [width];
+        for (i = 0; i<width; i++)
+            cords[i] = new unsigned short int [height];
+
+        for(i = 0;i<height;i++)
+        {
+            for(j = 0;j<width;j++)
+            {
+                tFile >> cords[i][j];
+
+            }
+
+
+        }
+
+        tFile >> n;
+
+        window = new unsigned short int * [n];
+        for (i = 0; i<n; i++)
+            window[i] = new unsigned short int [4];
+
+        for(i = 0;i<n;i++)
+        {
+            for(j = 0;j<4;j++)
+            {
+                tFile >> window[i][j];
+            }
+        }
+
+    }
+    tFile.close();
+
+    *pnHeight = height;
+    *pnNbWindow = n;
+    *pnWidth = width;
+    *pnWindow = window;
+    *pnCoord = cords;
 }
---
+++ parser.h
@@ -0,0 +1,8 @@
+#ifndef PARCER_H_
+#define PARCER_H_
+
+typedef unsigned short int USI;
+
+void readTable(USI ***cords,USI ***window, int *pnWidth,int *pnHeight,int *pnNbWindow);
+
+#endif // PARCER_H_
